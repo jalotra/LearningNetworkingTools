c4c8ee6d8685fe5d262423db4f58be5ac6611c54
Shivam <jalotrashivam9@gmail.com>
2019-07-08 15:37:28 +0530

    Edited this file

--- SearchingNetworks/all_ip_addresses_status.sh
+++ SearchingNetworks/all_ip_addresses_status.sh
@@ -5,3 +5,9 @@ ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
 
 # & sign is to use multithreading
+#example
+#./all_ip_addresses_status.sh 192.168.0
+#result
+#192.168.0.1
+#192.168.0.2
+#192.168.0.5
