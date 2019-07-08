ffd41d89954a9b0ba068e103d497a7532586d290
Shivam <jalotrashivam9@gmail.com>
2019-07-08 15:46:41 +0530

    Added if-else

--- SearchingNetworks/all_ip_addresses_status.sh
+++ SearchingNetworks/all_ip_addresses_status.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-for ip in `seq 1 255`;do
-ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-done
+if [ "$1" == "" ]
+then
+
+    echo "Syntax is wrong."
+    echo "Syntax:./all_ip_addresses_status.sh 192.168.0"
+
+else
+    for ip in `seq 1 255`;do
+    ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    done
+fi
 
 # & sign is to use multithreading
 #example
